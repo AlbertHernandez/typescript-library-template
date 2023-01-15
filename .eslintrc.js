d5685bd049e27f69a84a5cbf8dbf82f4e0aebb4a
Albert Hernandez <alberthernandezdev@gmail.com>
2023-01-15 10:14:50 +0100

    feat: improve linter rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,11 +4,16 @@ module.exports = {
     project: 'tsconfig.json',
     sourceType: 'module',
   },
-  plugins: ['@typescript-eslint/eslint-plugin'],
+  plugins: ['@typescript-eslint/eslint-plugin', 'simple-import-sort'],
   extends: [
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
   ],
+  rules: {
+    "simple-import-sort/imports": "error",
+    "simple-import-sort/exports": "error",
+    'prettier/prettier': ['error', { printWidth: 120 }],
+  },
   root: true,
   env: {
     node: true,
