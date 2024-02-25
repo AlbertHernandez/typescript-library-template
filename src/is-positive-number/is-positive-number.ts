461c0813fcedfc32f3ea5ab28b5dd893ab51715d
Albert <alberthernandezdev@gmail.com>
2024-02-25 21:38:39 +0100

    fix: change number (#104)

--- src/is-positive-number/is-positive-number.ts
+++ src/is-positive-number/is-positive-number.ts
@@ -1,5 +1,5 @@
 export const isPositiveNumber = (value: number): boolean => {
   // eslint-disable-next-line no-console
-  console.log("Hello world!");
+  console.log("Hello world 2!");
   return Math.sign(value) === 1;
 };
