5e0a468869128442ed1bbc0fa3ec2fff934c6a8c
Albert Hernandez Pellicer <alberto.pellicer@coverwallet.com>
2024-02-25 21:57:14 +0100

    fix: change number

--- src/is-positive-number/is-positive-number.ts
+++ src/is-positive-number/is-positive-number.ts
@@ -1,5 +1,5 @@
 export const isPositiveNumber = (value: number): boolean => {
   // eslint-disable-next-line no-console
-  console.log("Hello world 2!");
+  console.log("Hello world!");
   return Math.sign(value) === 1;
 };
