b1e2aeabeda38e72cebfc3f8ae5e9f5722e01d02
Albert Hernandez <alberthernandezdev@gmail.com>
2024-07-26 08:32:19 +0200

    chore(typos): create small functions

--- scripts/check_typos.sh
+++ scripts/check_typos.sh
@@ -1,37 +1,53 @@
 #!/bin/bash
 
-IGNORE_EXTENSIONS=("png" "snap" "jpg")
+check_typos_installed() {
+    if ! command -v typos >/dev/null 2>&1; then
+        echo "Typos CLI tool is not installed, aborting typo check."
+        echo "If you want to install it, you can run 'brew install typos-cli'"
+        exit 0 # We don't want to fail the build if the tool is not installed
+    fi
+}
 
-if ! command -v typos >/dev/null 2>&1; then
-    echo "Typos CLI tool is not installed, aborting typo check."
-    echo "If you want to install it, you can run 'brew install typos-cli'"
-    exit 0 # We don't want to fail the build if the tool is not installed
-fi
+get_files() {
+    if [ "$#" -eq 0 ]; then
+        echo "."
+    else
+        echo "$@"
+    fi
+}
 
-if [ "$#" -eq 0 ]; then
-    absolute_path_files="."
-else
-    absolute_path_files="$@"
-fi
+filter_files() {
+    IGNORE_EXTENSIONS=("png" "snap" "jpg")
 
-filtered_files=""
-for file in $absolute_path_files; do
-    ignore_file=false
-    for ext in "${IGNORE_EXTENSIONS[@]}"; do
-        if [[ $file == *.$ext ]]; then
-            ignore_file=true
-            break
+    local files="$1"
+    local filtered=""
+    for file in $files; do
+        ignore_file=false
+        for ext in "${IGNORE_EXTENSIONS[@]}"; do
+            if [[ $file == *.$ext ]]; then
+                ignore_file=true
+                break
+            fi
+        done
+        if [ "$ignore_file" = false ]; then
+            filtered="$filtered $file"
         fi
     done
-    if [ "$ignore_file" = false ]; then
-        filtered_files="$filtered_files $file"
-    fi
-done
+    echo "$filtered"
+}
 
-current_dir=$(pwd)
-files=""
-for file in $filtered_files; do
-    files="$files ${file#$current_dir/}"
-done
+convert_to_relative_paths() {
+    local files="$1"
+    local current_dir=$(pwd)
+    local relative=""
+    for file in $files; do
+        relative="$relative ${file#$current_dir/}"
+    done
+    echo "$relative"
+}
 
-typos $files
+check_typos_installed
+absolute_path_files=$(get_files "$@")
+filtered_files=$(filter_files "$absolute_path_files")
+relative_files=$(convert_to_relative_paths "$filtered_files")
+typos $relative_files
